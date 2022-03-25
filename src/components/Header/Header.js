0a22fbcb6b1929e19ab768191d9e170a4a2a58cf
Tohidul29 <tohidswfu@gmail.com>
2022-03-25 21:51:06 +0600

    main header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='main-header-container'>
+            <h1 className='main-header'>Our Sports World</h1>
+        </div>
+    );
+};
+
+export default Header;
